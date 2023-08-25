16af5d8997a8827bc8707ad21e525bd94fd48f5e
pranay101 <pranayprajapati101@gmail.com>
2023-08-25 17:38:09 +0530

    Finished responsive home page

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
    <main className='h-full w-screen bg-white text-black relative -mt-36'>
         <div className='text-center absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]'>
-        <span className='text-8xl font-bold text-gray-900'>Mann-Note</span>
+        <span className='px-3 text-8xl font-bold text-gray-900 md:whitespace-nowrap'>Mann-Note</span>
         <p className='text-gray-500 text-base tracking-wider mt-3'>"Empower Your Productivity: Where Notes and Plans Unite!"</p>
         <span className='w-full text-center'>
             <button className='flex items-center text-lg mx-auto mt-8 px-5 py-1 rounded-lg font-semibold border-2 hover:bg-gray-900 hover:text-white'>Get Started 
@@ -15,7 +15,7 @@ export default function Home() {
         </span>
         </div>
         {/* <Image className='absolute bottom-0 left-10 opacity-50' src={"/background.svg"}height={500} width={500} alt='productivity' /> */}
-        <div className='border-t h-20 w-full absolute bottom-0 left-0 flex justify-between text-sm text-gray-500 items-center px-24'>
+        <div className='border-t h-20 w-full absolute bottom-0 left-0 flex justify-between text-sm text-gray-500 items-center px-8 md:px-24'>
             <span>
                 <Link href={""}>Share Feedback</Link>
             </span>
