51c8bde79e2e03d21699963bbbc78c16578e7c44
pranay101 <pranayprajapati101@gmail.com>
2023-08-27 09:25:39 +0530

    Fixed Navbar

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -8,7 +8,7 @@ import { useState } from 'react'
 type Props = {}
 
 const Navbar = (props: Props) => {
-    // const [openMenu,setOpenMenu] = useState(false)
+    const [openMenu,setOpenMenu] = useState(false)
     return (<>
     <nav className='absolute h-36 top-0 left-0 w-screen px-5 md:px-20 text-black flex justify-between items-center z-50 overflow-x-hidden'>
             <Image className='' src={"/Mannotelogo.svg"} height={100} width={150} alt='logo' />
@@ -18,10 +18,10 @@ const Navbar = (props: Props) => {
                 <span className='flex items-center mx-5 px-5 py-1.5 rounded-[5px] font-semibold border-2 hover:bg-gray-900 hover:text-white'><Link href="">Login</Link></span>
             </div>
            
-            <Bars3Icon className='md:hidden h-10 w-10 fill-gray-900' />
+            <Bars3Icon onClick={() => setOpenMenu(true)} className='md:hidden h-10 w-10 fill-gray-900' />
         </nav>
-         <div className={`md:hidden absolute h-screen w-screen bg-gray-50 z-50  ease-out duration-500`}>
-                <XMarkIcon className='h-10 w-10 fill-gray-900 absolute top-10 right-5 z-50 '  />
+         <div className={`md:hidden absolute h-screen w-screen bg-gray-50 z-50  ease-out duration-500 ${!openMenu? "translate-x-[100%]" : "translate-x-0" }`}>
+                <XMarkIcon onClick={() => setOpenMenu(false)}className='h-10 w-10 fill-gray-900 absolute top-10 right-5 z-50 '  />
         
                 <div className='h-full w-full flex items-center justify-center flex-col'>
                 <Link className='border-b-2 text-lg border-transparent hover:border-b-2 hover:border-gray-300 my-5' href={""}>Pricing</Link>
