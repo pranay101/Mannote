a9a2bf020efe5e3017894c52f2f99149feea16e7
pranay101 <pranayprajapati101@gmail.com>
2023-09-06 19:22:28 +0530

    saving board name, app restructure, and userschema

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -1,26 +1,36 @@
 'use client'
 import Canvas from '@/src/Canvas/Canvas'
 import { useEffect, useState } from 'react'
-import { cardsAtom } from '@/Config/RecoilConfig'
+import { cardsAtom,userAtom } from '@/Config/RecoilConfig'
 import { useRecoilState, useRecoilValue } from 'recoil'
 import { CardProps } from '@/Config/typings'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { Button } from '@mui/material'
+import { useRouter } from 'next/navigation'
+
 type Props = {}
 
 const page = (props: Props) => {
-    const [cards, setCards] = useRecoilState(cardsAtom);  
-    const [lastSaveTime, setLastSaveTime] = useState("")
+    const [cards, setCards] = useRecoilState(cardsAtom)
+    const [user, setUser] = useRecoilState(userAtom)
+    const [lastSaveTime, setLastSaveTime] = useState('Never')
+    const router = useRouter();
+
+    const saveDataToLocalStorage = (data: CardProps[]) => {
+        const currentTime = new Date().toLocaleTimeString()
+        localStorage.setItem('cardData', JSON.stringify(data))
+        localStorage.setItem('lastSaveTime', currentTime)
+        return currentTime
+    }
     
-    const saveDataToLocalStorage = (data:CardProps[]) => {
-        const currentTime = new Date().toLocaleTimeString();
-        localStorage.setItem('cardData', JSON.stringify(data));
-        localStorage.setItem('lastSaveTime', currentTime);
-        return currentTime;
-      }
+    const updateBoardTitle = (boardTitle:string) =>{
+        localStorage.setItem('boardTitle', boardTitle)
+    }
 
     useEffect(() => {
         // Save data to local storage every 10 seconds
         const saveInterval = setInterval(() => {
-            console.log("Saving...");
+            console.log('Saving...')
             const currentTime = saveDataToLocalStorage(cards)
             setLastSaveTime(currentTime)
         }, 10000)
@@ -31,32 +41,49 @@ const page = (props: Props) => {
 
     useEffect(() => {
         // Get data from local storage
-        const savedData = localStorage.getItem('cardData');
-        const lastSavedTimeLocal = localStorage.getItem('lastSaveTime') || "";
+        const savedData = localStorage.getItem('cardData')
+        const lastSavedTimeLocal = localStorage.getItem('lastSaveTime') || ''
+        const boardTitle = localStorage.getItem('boardTitle') || ''
         if (savedData) {
-          const parsedData = JSON.parse(savedData);          
-          setCards(parsedData);
+            const parsedData = JSON.parse(savedData)
+            setCards(parsedData)
         }
+        setUser(userDetails =>{
+            return {
+                ...userDetails,
+                moodBoardTitle:boardTitle
+            }
+        })
         setLastSaveTime(lastSavedTimeLocal)
-      }, [setCards]);
+    }, [setCards])
 
     return (
-        <main className="h-screen w-screen bg-gray-100 grid overflow-hidden relative">
-            <section className="w-full h-[10vh] bg-gray-800 flex justify-center items-center">
-                <h1
-                    contentEditable
-                    suppressContentEditableWarning
-                    className="text-white text-lg focus:outline-none focus:border-b focus:border-b-gray-400 w-fit mx-auto"
-                >
-                    Mood Board Title
-                </h1>
-                <p title='Saves Every minute' className='absolute top-5 right-3 text-gray-50 text-sm'>Last Saved: {lastSaveTime}</p>
-            </section>
-            <section className="flex h-[90vh] w-full overflow-hidden">
-                <Canvas />
-            </section>
-        </main>
+            <main className="h-screen w-screen bg-gray-100 grid overflow-hidden relative">
+                <section className="w-full h-[10vh] bg-gray-800 flex justify-between items-center px-5">
+                  
+                        <Button onClick={() => router.push("/")} variant='text' color='info' startIcon={<ArrowBackIcon/>} className='text-white'>Home</Button>
+                    <h1
+                        contentEditable
+                        suppressContentEditableWarning
+                        className="text-white text-lg focus:outline-none focus:border-b focus:border-b-gray-400 w-fit mx-auto"
+                        onBlur={(e) =>
+                            updateBoardTitle(e.target.innerText)
+                        }
+                    >
+                        {user.moodBoardTitle}
+                    </h1>
+                    <p
+                        title="Saves Every minute"
+                        className="text-gray-50 text-sm"
+                    >
+                        Last Saved: {lastSaveTime}
+                    </p>
+                </section>
+                <section className="flex h-[90vh] w-full overflow-hidden">
+                    <Canvas />
+                </section>
+            </main>
     )
 }
 
-export default page;
+export default page
--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -2,22 +2,24 @@
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 
 
 type Props = {}
 
 const Navbar = (props: Props) => {
-    const [openMenu,setOpenMenu] = useState(false)
+    const [openMenu, setOpenMenu] = useState(false)
+    const router = useRouter()
     return (
         <>
             <nav className="absolute h-36 top-0 left-0 w-screen px-5 md:px-20 text-black flex justify-between items-center z-50 overflow-hidden">
                 <Image
-                    
-                    className=""
+                    onClick={() => router.push('/')}
+                    className="cursor-pointer"
                     src={'/MannoteLogo.png'}
-                    height={100}
-                    width={150}
+                    height={50}
+                    width={100}
                     alt="logo"
                 />
                 <div className="hidden md:flex text-sm uppercase font-normal text-black tracking-widest items-center">
@@ -33,9 +35,17 @@ const Navbar = (props: Props) => {
                     >
                         About
                     </Link>
+                    <Link
+                        className="border-b-2 border-transparent hover:border-b-2 hover:border-gray-300 mx-5"
+                        href={'/dashboard'}
+                    >
+                        Dashboard
+                    </Link>
+                    <Link href="/Login">
                         <span className="flex items-center mx-5 px-5 py-1.5 rounded-[5px] font-semibold border-2 hover:bg-gray-900 hover:text-white">
-                            <Link href="/Login">Login</Link>
+                            Login
                         </span>
+                    </Link>
                 </div>
 
                 <Bars3Icon
