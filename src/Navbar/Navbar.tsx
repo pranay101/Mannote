bdca1595c076b6fc0b05ca85d1a1660ab9f689e1
pranay101 <pranayprajapati101@gmail.com>
2023-09-06 19:31:33 +0530

    Fixed Bugs

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -37,7 +37,7 @@ const Navbar = (props: Props) => {
                     </Link>
                     <Link
                         className="border-b-2 border-transparent hover:border-b-2 hover:border-gray-300 mx-5"
-                        href={'/dashboard'}
+                        href={'/Dashboard'}
                     >
                         Dashboard
                     </Link>
@@ -78,7 +78,7 @@ const Navbar = (props: Props) => {
                     </Link>
                     <Link
                         className="border-b-2 text-lg border-transparent hover:border-b-2 hover:border-gray-300 my-5"
-                        href={'/dashboard'}
+                        href={'/Dashboard'}
                     >
                         Dashboard
                     </Link>
